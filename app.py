5c92b21781833bee09c46a83e729235a0228f0d8
David Olivares <olivaresosuna@gmail.com>
2026-03-06 10:34:14 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="NOVAres | Amazon Beauty Intelligence",
+    layout="wide"
+)
+
+st.title("NOVAres Amazon Beauty Dashboard")
+st.write("Dashboard setup in progress.")
